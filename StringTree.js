a1481ae564821a6ed95ce177386522096a12f8a8
Brian Putnam <brian@brian-putnam.com>
2015-07-16 18:51:53 -0600

    Added minKey/maxKey fns, shadowRoot, tests

--- StringTree.js
+++ StringTree.js
@@ -1,36 +1,101 @@
 (function(globals) {
 
 	function StringTree(comparator) {
-		this.comparator = comparator;
+		this.comparator = comparator || StringTree.defaultComparator;
 		this.root = {};
+		this._shadowRoot = {};
 	}
 
+	StringTree.defaultComparator = function(a, b) {
+		a = parseInt(a);
+		b = parseInt(b);
+		if (a < b) { return -1; }
+		else if (b < a) { return 1; }
+		else { return 0; }
+	};
+
 	StringTree.prototype.get = function() {
-		var root = this.root;
-		for (var i=0; i<arguments.length; i++) {
-			var key = arguments[i];
-			if (root.hasOwnProperty(key)) {
-				root = root[key];
-			}
-			else {
-				return undefined;
-			}
+		if (arguments.length < 1) {
+			throw "StringTree.get requires at least 1 parameter";
 		}
-		return root;
+		else {
+			var root = this.root;
+			var shadowRoot = this._shadowRoot;
+
+			for (var i=0; i<arguments.length; i++) {
+				var key = arguments[i];
+				if (shadowRoot.hasOwnProperty(key)) {
+					shadowRoot = shadowRoot[key];
+					root = root[key];
+				}
+				else {
+					return undefined;
+				}
+			}
+			return root;
+		}
 	};
 
 	StringTree.prototype.set = function() {
-		var root = this.root;
-		var value = arguments[arguments.length-1];
-		var lastKey = arguments[arguments.length-2];
-		for (var i=0; i<arguments.length-2; i++) {
-			var key = arguments[i];
-			if (!root.hasOwnProperty(key)) {
-				root[key] = {};
-			}
-			root = root[key];
+		if (arguments.length < 2) {
+			throw "StringTree.set requires at least 2 parameters";
 		}
-		root[lastKey] = value;
+		else {
+			var root = this.root;
+			var shadowRoot = this._shadowRoot;
+
+			var value = arguments[arguments.length-1];
+			var lastKey = arguments[arguments.length-2];
+			for (var i=0; i<arguments.length-2; i++) {
+				var key = arguments[i];
+				if (!shadowRoot.hasOwnProperty(key)) {
+					shadowRoot[key] = {};
+					root[key] = {};
+				}
+				root = root[key];
+				shadowRoot = shadowRoot[key];
+			}
+			root[lastKey] = value;
+			shadowRoot[lastKey] = {};
+		}
+	};
+
+	StringTree.prototype._getSortedKeys = function(obj) {
+		return Object.keys(obj).sort(this.comparator);
+	};
+
+	StringTree.prototype.maxKey = function() {
+		var value = this.get.apply(this, arguments);
+
+		var keys = Object.keys(value);
+		if (keys.length===0) {
+			return undefined;
+		}
+
+		var maxKey = keys[0];
+		for (var i=1; i<keys.length; i++) {
+			if (this.comparator(keys[i], maxKey) > 0) {
+				maxKey = keys[i];
+			}
+		}
+		return maxKey;
+	};
+
+	StringTree.prototype.minKey = function() {
+		var value = this.get.apply(this, arguments);
+
+		var keys = Object.keys(value);
+		if (keys.length===0) {
+			return undefined;
+		}
+
+		var minKey = keys[0];
+		for (var i=1; i<keys.length; i++) {
+			if (this.comparator(keys[i], minKey) < 0) {
+				minKey = keys[i];
+			}
+		}
+		return minKey;
 	};
 
 	if( typeof exports !== 'undefined' ) {
--- test/StringTree.spec.js
+++ test/StringTree.spec.js
@@ -5,11 +5,82 @@ if( typeof StringTree === 'undefined' ) {
 describe('StringTree', function(){
 
 	it('must get and set values', function(){
-
 		var st = new StringTree();
 		st.set("foo", "bar", "baz", {hello: "world"});
 
 		expect(st.get("foo", "bar", "baz")).toEqual({hello: "world"});
+	});
+
+	it("should return undefined when key is missing", function() {
+		var st = new StringTree();
+		st.set("foo", "bar", "baz", {hello: "world"});
+		expect(typeof st.get("foo", "bar", "baz", "bat")).toEqual("undefined");
+		expect(typeof st.get("goo", "bar", "baz")).toEqual("undefined");
+	});
+
+	it("must sort numeric values correctly", function() {
+		var st = new StringTree();
+		st.set("foo", {
+			"01": {val: 1},
+			"03": {val: 3},
+			"02": {val: 2},
+			"10": {val: 10}
+		});
+
+		expect(st._getSortedKeys(st.get("foo"))).toEqual(["01", "02", "03", "10"]);
+	});
+
+	it("must get max key correctly", function() {
+		var st = new StringTree();
+		st.set("foo", {
+			"01": {val: 1},
+			"03": {val: 3},
+			"02": {val: 2},
+			"10": {val: 10}
+		});
+
+		expect(st.maxKey("foo")).toEqual("10");
+	});
+
+	it("must get min key correctly", function() {
+		var st = new StringTree();
+		st.set("foo", {
+			"01": {val: 1},
+			"03": {val: 3},
+			"02": {val: 2},
+			"10": {val: 10}
+		});
+
+		expect(st.minKey("foo")).toBe("01");
+	});
+
+	it("must throw when less than 2 params are passed to set", function() {
+		var st = new StringTree();
+
+		try {
+			st.set();
+			console.log("FOO");
+			fail();
+		}
+		catch (e) { /* pass */ }
+
+		try {
+			st.set(1);
+			console.log("BAR");
+			fail();
+		}
+		catch (e) { /* pass */ }
+	});
+
+	it("must throw when less than 1 param is passed to get", function() {
+		var st = new StringTree();
+		st.set("foo", 1);
+
+		try {
+			st.get();
+			fail();
+		}
+		catch (e) { /* pass */ }
 	});
 
 });
